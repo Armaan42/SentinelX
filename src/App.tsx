55079d21635e82ac9bccf3a3c344f775c45aa1d2
Armaan <armaansinghbhau8@gmail.com>
2026-01-12 23:32:05 +0530

    Added route paths

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import Index from "@/pages/Index";
 import Demo from "./pages/Demo";
+import FeatureDetail from "./pages/FeatureDetail";
+import ProblemDetail from "@/pages/ProblemDetail";
+import ComparisonPage from "./pages/ComparisonPage";
 import NotFound from "./pages/NotFound";
+import AnalyticsDetail from "./pages/AnalyticsDetail";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +22,10 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/demo" element={<Demo />} />
+          <Route path="/feature/:featureId" element={<FeatureDetail />} />
+          <Route path="/problem/:problemId" element={<ProblemDetail />} />
+          <Route path="/comparison" element={<ComparisonPage />} />
+          <Route path="/analytics/:analysisId" element={<AnalyticsDetail />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
